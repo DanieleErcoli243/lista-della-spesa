9bc7944d153523ac63a8e747709f2eae01220d76
danieleercoli243 <danielercoli@gmail.com>
2025-10-30 12:31:40 +0100

    restored the transparent colour of the body and added items to the data

--- js/script.js
+++ js/script.js
@@ -1,1 +1,14 @@
 console.log("vue OK", Vue);
+
+const {createApp} = Vue;
+const app = createApp ({
+    data: () => ({
+        items: [
+            {id: 1, label: '10 cappelli da festa'},
+            {id: 2, label: '2 giochi da tavolo'},
+            {id: 3, label: '20 bicchieri'}
+        ]
+    })
+})
+
+app.mount("#app");
